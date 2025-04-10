a0e0acade31d8e6ad57402d5ad1d9beb569adf59
ejunior89 <eliomaiajunior@gmail.com>
2025-04-10 08:40:57 -0400

    .

--- arquiteturaspring/src/main/java/io/github/ejunior89/arquiteturaspring/montadora/api/TesteFabricaController.java
+++ arquiteturaspring/src/main/java/io/github/ejunior89/arquiteturaspring/montadora/api/TesteFabricaController.java
@@ -1,15 +1,21 @@
 package io.github.ejunior89.arquiteturaspring.montadora.api;
 
-import io.github.ejunior89.arquiteturaspring.montadora.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.github.ejunior89.arquiteturaspring.montadora.Aspirado;
+import io.github.ejunior89.arquiteturaspring.montadora.CarroStatus;
+import io.github.ejunior89.arquiteturaspring.montadora.Chave;
+import io.github.ejunior89.arquiteturaspring.montadora.HondaHRV;
+import io.github.ejunior89.arquiteturaspring.montadora.Motor;
+
 @RestController
 public class TesteFabricaController {
 
     @Autowired
+    @Aspirado
     private Motor motor;
 
     @PostMapping("/carros")
